e3c094e7f54022120a40046af8ab4800db986fc4
Proman21 <snoopyk921@gmail.com>
2013-03-07 16:51:03 +1000

    Initial Build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freakingrecipes::Application.config.secret_token = '138c91dd3bae33a6e0163c3ffd733bf9628f6f5bb7ea2f5797158092e66d4125162663e0dcb0811550fb477fe0a3eb1e1f16486056c668faead26926a98b038e'
